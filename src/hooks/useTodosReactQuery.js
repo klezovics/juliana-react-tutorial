f1172ac02a94881d204958029a18a6755a907ddf
klezovich <arthur@embeddable.de>
2023-10-16 03:18:00 +0200

    Add react todos

--- src/hooks/useTodosReactQuery.js
+++ src/hooks/useTodosReactQuery.js
@@ -2,6 +2,10 @@ import {useQuery} from "react-query";
 import {fetchTodos} from "../api/todosApi";
 
 // notice how little boilerplate we have
+/*
+    This is how you use it
+    const { data, isLoading, isError, error } = useTodos();
+ */
 export const useTodosReactQuery = () => {
     return useQuery('todos', fetchTodos);
 };
