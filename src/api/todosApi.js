bcabaef0201187ab5ed71fe08ed266f32997b5e8
klezovich <arthur@embeddable.de>
2023-10-16 03:21:48 +0200

    Add superagent example

--- src/api/todosApi.js
+++ src/api/todosApi.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import request from "superagent";
 
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
@@ -6,6 +7,30 @@ export const fetchTodos = async () => {
     try {
         const response = await axios.get(apiUrl);
         return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const fetchTodosWithFetchApi = async () => {
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const fetchTodosUsingSuperagent = async () => {
+    try {
+        const response = await request.get(apiUrl);
+        return response.body;
     } catch (error) {
         throw error;
     }
