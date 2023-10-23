ad5f8f5674263ed7052ef7a85e35000c6cafabe6
klezovich <arthur@embeddable.de>
2023-10-23 18:34:48 +0200

    Add styled components homework

---
+++ src/pages/homework/styled_components.js
@@ -0,0 +1,7 @@
+/*
+     Create a component for each of the tasks
+     1. Code for a styled component using styled.`div` or similar
+     2. Demonstrate use of inheritance in styled components
+     3. Demonstrate use of props and conditional styling in styled components
+     4. Watch a styled components tutorial
+ */
