ad306d9e1e5c1e480c03f8292c5d6f94ecaa8c96
klezovich <arthur@embeddable.de>
2023-10-26 12:20:11 +0200

    Using react query to generate data from ordinary function

---
+++ src/pages/rq/from_file.js
@@ -0,0 +1,23 @@
+import {useQuery} from "react-query";
+
+const dataProvider = () => {
+    return [1,2,3,4]
+}
+
+export const Page = () => {
+
+    const {data, isLoading, isError} = useQuery("my-data", dataProvider)
+
+    return (
+        <>
+            <h1>Welcome to RQ!</h1>
+            <ul>
+                {data && data.map((item) => (
+                    <li key={item}>{item}</li>
+                ))}
+            </ul>
+        </>
+    )
+}
+
+export default Page
